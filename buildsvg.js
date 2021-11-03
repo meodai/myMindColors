931a1ccc77684259de03a545620aa7755e3df0f2
David Aerne <meodai@gmail.com>
2021-11-03 21:51:58 +0100

    docs(preview): adds svg preview

--- buildsvg.js
+++ buildsvg.js
@@ -5,14 +5,14 @@ const w = 200;
 const itemH = 30;
 
 const svg = `
-  <svg viewBox="0 0 ${w} ${items * itemH}">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${w} ${items * itemH}">
     ${colors.map((c, i) => {
       const width = w/c.colors.length;
 
       return c.colors.map((hex, j) => {
         return `<rect width="${width}" height="${itemH}" stroke="#fff" y="${i * itemH}" x="${j * width}" fill="${hex}" />`
-      }).join('');
-    }).join('')}
+      }).join('\n');
+    }).join('\n')}
   </svg>
 `;
 
