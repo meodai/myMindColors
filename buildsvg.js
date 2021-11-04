4fff97e3be48bda385bea0b0398625857a516c40
David Aerne <meodai@gmail.com>
2021-11-04 12:04:51 +0100

    try to fix selects for windows

--- buildsvg.js
+++ buildsvg.js
@@ -3,14 +3,17 @@ const colors = require('.');
 const items = colors.length;
 const w = 200;
 const itemH = 30;
+const padding = 10;
+const h = ( itemH + padding * 2 ) * items;
 
 const svg = `
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${w} ${items * itemH}">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${w} ${h}">
     ${colors.map((c, i) => {
       const width = w/c.colors.length;
+      const top = i * (itemH + padding * 2);
 
       return c.colors.map((hex, j) => {
-        return `<rect width="${width}" height="${itemH}" stroke="#fff" y="${i * itemH}" x="${j * width}" fill="${hex}" />`
+        return `<rect width="${width}" height="${itemH}" stroke="#fff" y="${top}" x="${j * width}" fill="${hex}" />`
       }).join('\n');
     }).join('\n')}
   </svg>
